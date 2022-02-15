46384f7bbac13e929a8955b322cdfb54333a389c
Muktida9 <97612499+Muktida9@users.noreply.github.com>
2022-02-15 14:11:46 +0700

    Update mypage2.php

--- mypage2.php
+++ mypage2.php
@@ -3,6 +3,6 @@
     <title>เว็บไซต์ของ มุกธิดา จันที</title>
   </head>
   <body>
-    <ahref="mypage1.php"><h1>mypage1</h1></a>
+    <a href="mypage1.php"><h1>mypage1.php</h1></a>
   </body>
 </html>
