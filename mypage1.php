4ff279c34b2089a999b50bc6c7ae4b02647b70db
Muktida9 <97612499+Muktida9@users.noreply.github.com>
2022-02-15 14:10:54 +0700

    Update mypage1.php

--- mypage1.php
+++ mypage1.php
@@ -3,7 +3,7 @@
     <title>เว็บไซต์ของ มุกธิดา จันที</title>
   </head>
   <body>
-    <ahref="https://facebook.com"><h1>facebook</h1></a>
-    <ahref="mypage2.php"><h1>mypage2</h1></a>
+    <a href="https://facebook.com"><h1>facebook</h1></a>
+    <a href="mypage2.php"><h1>mypage2.php</h1></a>
   </body>
 </html>
