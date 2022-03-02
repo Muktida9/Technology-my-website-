251b7f526c734d845a1ff50713546743a39bcfdd
Muktida9 <97612499+Muktida9@users.noreply.github.com>
2022-03-02 20:00:08 +0700

    Create table1.php

---
+++ table1.php
@@ -0,0 +1,45 @@
+<html>
+  <head>
+    <title>การสร้างตาราง</title>
+  </head>
+  <body>
+    <table border="1">
+      <tr>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>ตูน บอดี้สแลม</td>
+        <td><img src="https://www.newtv.co.th/images/content/ct_20191218030826290.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/koytoonfamily">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>โตโน่ ภาคิน</td>
+        <td><img src="https://s.isanook.com/ns/0/ui/377/1887155/b3a31747641dab20fe074207c153902c_1445537475.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/profile.php?id=100044293906357">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>ออกัส วชิรวิชญ์</td>
+        <td><img src="https://image.sistacafe.com/images/uploads/summary/image/65471/52993449_604936606641362_3719298101253187180_n.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/aaugustvachiravit007/">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>มิว ชิษณุชา</td>
+        <td><img src="https://static.trueplookpanya.com/tppy/member/m_545000_547500/545920/cms/images/w644.jpg" width="45px"></td>
+        <td><a href="https://www.facebook.com/meyoumeyouu">Facebook</a></td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>เจมส์ จิรายุ</td>
+        <td><img src="https://mpics.mgronline.com/pics/Images/564000005619001.JPEG" width="45px"></td>
+        <td><a href="https://www.facebook.com/TangsrisukJJ">Facebook</a></td>
+      </tr>
+    </table>
+  </body>
+</html>
